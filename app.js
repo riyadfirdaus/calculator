40cc0cd605a76ca0704005b10db8ec418fd1f829
Riyad Firdaus <riyadf27@gmail.com>
2022-10-27 06:25:47 +0700

    fix operator change state bug

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let numberStr = '';
 let operationStr = '';
-let sum = 0;
+let sum;
 let isFirstNumber = true;
 let operator;
 let isFinished = true;
+let digitPressed, equalPressed, operatorPressed;
+let state = 'clear';
 
 const digits = document.querySelectorAll('.digits');
 const operators = document.querySelectorAll('.operators');
@@ -12,7 +14,7 @@ const resultDisplay = document.querySelector("#resultDisplay");
 const clearBtn = document.querySelector("#clear");
 const equalBtn = document.querySelector('#equal');
 
-clearBtn.addEventListener('click', clearDisplay);
+clearBtn.addEventListener('click', clearCalc);
 digits.forEach(digit =>{
     digit.addEventListener('click', append);
 });
@@ -21,15 +23,14 @@ operators.forEach(operator =>{
 })
 equalBtn.addEventListener('click', equal);
 
-function clearDisplay(){
-    isFinished = false;
+function clearCalc(){
     operationStr = '';
     numberStr = '';
     sum = 0;
     operand = 0;
     operationDisplay.textContent = operationStr;
     resultDisplay.textContent = operationStr;
-    isFirstNumber = true;
+    state = 'clear';
 }
 function updateDisplay(str){
     operationDisplay.textContent = operationStr;
@@ -37,59 +38,68 @@ function updateDisplay(str){
 }
 
 function append(e){
-    if(isFinished){
-        clearDisplay();
+    const value = e.target.textContent;
+    console.log(operationStr);
+    if(state == 'equal'){
+        clearCalc();
     }
-    numberStr += e.target.textContent;
-    operationStr += e.target.textContent;
+    
+    numberStr += value;
+    operationStr += value;
+    
     operand = parseInt(numberStr);
     updateDisplay(operand);
-    isFinished = false;
+    state = 'digit'
 }
 
 function calculate(sum, operand, operator){
     switch(operator){
         case 'divide':
-            if(operand === 0){
-                return 'error';
-            }
-            return sum*1.0 / operand;
-            break;
+        if(operand === 0){
+            return 'error';
+        }
+        return sum*1.0 / operand;
+        break;
         case 'multiply':
-            return sum * operand;
-            break;
+        return sum * operand;
+        break;
         case 'plus':
-            return sum + operand;
-            break;
+        return sum + operand;
+        break;
         case 'minus':
-            return sum - operand;
-            break;
+        return sum - operand;
+        break;
     }
 }
 function assign(e){
+    if(state == 'operator'){
+        operationStr = operationStr.slice(0,-3);
+    }
+    
+    operator = (e.target.id);
     operationStr += ` ${e.target.textContent} `;
     numberStr = '';
-    if(isFirstNumber){
+    if((state == 'digit' || state == 'equal') && sum == undefined){
         sum = operand;
-        isFirstNumber = false;
-        updateDisplay('');
+        updateDisplay(sum);
     }
-    else{
+    else if(state != 'operator'){
         sum = calculate(sum, operand, operator);
         updateDisplay(sum);
     }
-    operator = (e.target.id);
-    isFinished=false;
+    else{
+        updateDisplay(sum);
+    }
+    state = 'operator';
 }
 function equal(){
-    if(isFinished == false){
+    if(state != 'equal'){
         sum = calculate(sum, operand, operator);
         updateDisplay(sum);
     }
     operator = '';
     operationStr = sum;
-    isFinished = true;
-    isFirstNumber = true;
     operand = sum;
+    sum = undefined;
+    state = 'equal';
 }
-console.log('text');

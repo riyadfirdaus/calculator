69ffade7a3da0743bc2179086714ce79b9680f09
Riyad Firdaus <riyadf27@gmail.com>
2022-10-27 17:08:20 +0700

    add keyboard support

--- app.js
+++ app.js
@@ -7,53 +7,8 @@ const operators = document.querySelectorAll('.operators');
 const operationDisplay = document.querySelector("#operationDisplay");
 const resultDisplay = document.querySelector("#resultDisplay");
 const clearBtn = document.querySelector("#clear");
-const equalBtn = document.querySelector('#equal');
+const equalBtn = document.querySelector("#equal");
 
-clearBtn.addEventListener('click', clearCalc);
-digits.forEach(digit =>{
-    digit.addEventListener('click', append);
-});
-operators.forEach(operator =>{
-    operator.addEventListener('click', assign);
-})
-equalBtn.addEventListener('click', equal);
-
-function clearCalc(){
-    operationStr = '';
-    numberStr = '';
-    let sum;
-    operand = 0;
-    operationDisplay.textContent = operationStr;
-    resultDisplay.textContent = operationStr;
-    state = 'clear';
-}
-function updateDisplay(str){
-    if(str.toString().length>9){
-        console.log('whay');
-        number = new Intl.NumberFormat("en-US", {notation: "scientific"}).format(parseInt(str));
-    }
-    else 
-        number = new Intl.NumberFormat("en-US").format(parseInt(str));
-    operationDisplay.textContent = operationStr;
-    resultDisplay.textContent = number;
-}
-
-function append(e){
-    const value = e.target.textContent;
-    console.log(operationStr);
-    if(state == 'equal'){
-        clearCalc();
-    }
-    if(operand == 0){
-        operationStr = operationStr.slice(0,-1);
-    }
-        numberStr += value;
-        operationStr += value;
-
-    operand = parseInt(numberStr);
-    updateDisplay(operand);
-    state = 'digit'
-}
 
 function calculate(sum, operand, operator){
     switch(operator){
@@ -67,6 +22,7 @@ function calculate(sum, operand, operator){
         return sum * operand;
         break;
         case 'plus':
+        console.log(sum + operand);
         return sum + operand;
         break;
         case 'minus':
@@ -74,24 +30,58 @@ function calculate(sum, operand, operator){
         break;
     }
 }
+function clearCalc(){
+    operationStr = '';
+    numberStr = '';
+    sum = undefined;
+    operand = 0;
+    operationDisplay.textContent = operationStr;
+    resultDisplay.textContent = operationStr;
+    state = 'clear';
+}
+function updateDisplay(str){
+    if(str === undefined) return;
+    if(str.toString().length>9){
+        number = new Intl.NumberFormat("en-US", {notation: "scientific"}).format(parseInt(str));
+    }
+    else 
+    number = new Intl.NumberFormat("en-US").format(parseInt(str));
+    operationDisplay.textContent = operationStr;
+    resultDisplay.textContent = number;
+}
+
+function append(e){
+    const value = e.target.textContent;
+    if(state == 'equal'){
+        clearCalc();
+    }
+    if(operand == 0){
+        operationStr = operationStr.slice(0,-1);
+    }
+    numberStr += value;
+    operationStr += value;
+    
+    operand = parseInt(numberStr);
+    updateDisplay(operand);
+    state = 'digit'
+}
+
 function assign(e){
+    if(state == 'clear') return;
     if(state == 'operator'){
         operationStr = operationStr.slice(0,-3);
     }
     operator = (e.target.id);
     operationStr += ` ${e.target.textContent} `;
     numberStr = '';
+    
     if((state == 'digit' || state == 'equal') && sum == undefined){
         sum = operand;
-        updateDisplay(sum);
     }
-    else if(state != 'operator'){
+    else if(state != 'operator' && state != 'equal'){
         sum = calculate(sum, operand, operator);
-        updateDisplay(sum);
     }
-    else{
-        updateDisplay(sum);
-    }
+    updateDisplay(sum);
     state = 'operator';
 }
 function equal(){
@@ -100,8 +90,46 @@ function equal(){
         updateDisplay(sum);
     }
     operator = '';
-    operationStr = sum;
+    operationStr = `${sum}`;
     operand = sum;
-    sum = undefined;
+    console.log(sum);
     state = 'equal';
 }
+
+clearBtn.addEventListener('click', clearCalc);
+digits.forEach(digit =>{
+    digit.addEventListener('click', append);
+});
+operators.forEach(operator =>{
+    operator.addEventListener('click', assign);
+})
+equalBtn.addEventListener('click', equal);
+
+window.addEventListener('keydown', (e) =>{
+    let selector;
+    console.log(e.key);
+    switch(e.key){
+        case '-':
+            selector = '#minus';
+            break;
+        case '+':
+            selector = '#plus';
+            break;
+        case '/':
+            selector = '#divide';
+            break;
+        case '*':
+            selector = '#multiply';
+            break;
+        case 'Enter':
+            selector = '#equal';
+            break;
+        default:
+            selector = `#digit-${e.key}`;
+            break;
+        
+    }
+    const button = document.querySelector(`${selector}`);
+    if(button!= null)
+        button.click();
+});
